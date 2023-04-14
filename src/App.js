d410db1cc9f6cf4cbc6d57ab007e1c623a861e6b
Harsh Kumar Singh <114430264+Harshkrsingh05@users.noreply.github.com>
2023-04-14 22:57:00 +0530

    added footer

--- src/App.js
+++ src/App.js
@@ -8,6 +8,12 @@ function App() {
        <div className="container my-3">
         <TextForm heading="Enter text here."/>
        </div>
+       <footer className="bg-light text-center text-lg-start">
+          <div className="text-center p-3">
+            © 2023 Copyright:
+            <a className="text-dark" href="https://github.com/Harshkrsingh05"> HarshProduction</a>
+          </div>
+       </footer>
     </>
   );
 }
